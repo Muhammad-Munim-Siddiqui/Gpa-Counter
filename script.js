97de3247fa6eaf911bae8c4399aed2f94bcda87c
Muhammad Munim Siddiqui <munim1siddiqui@gmail.com>
2024-08-10 23:14:25 -0700

    complete script

---
+++ script.js
@@ -0,0 +1,50 @@
+var marks = +prompt("what is your Marks?");
+
+if(marks >=90 && marks <=100){
+    document.write("Your Grade is A+ ");
+document.write("Your GPA IS 4.0 !");
+}
+if(marks <= 89 && marks >= 85 ){
+    document.write("Your Grade is A ");
+    document.write("Your GPA IS 4.0 !");
+}
+if(marks <= 84 && marks >= 80){
+    document.write("Your Grade is A- ");
+    document.write("Your GPA IS 3.8 !");
+}
+if(marks <= 79 && marks >= 75){
+    document.write("Your Grade is B+ ");
+    document.write("Your GPA IS 3.4 !");
+}
+if(marks <= 74 && marks >= 71){
+    document.write("Your Grade is B ");
+    document.write("Your GPA IS 3.0 !");
+}
+if(marks <= 70 && marks >= 68){
+    document.write("Your Grade is B- ");
+    document.write("Your GPA IS 2.8 !");
+}
+if(marks <= 67 && marks >= 64){
+    document.write("Your Grade is C+ ");
+    document.write("Your GPA IS 2.4 !");
+}
+if(marks <= 63 && marks >= 61){
+    document.write("Your Grade is C ");
+    document.write("Your GPA IS 2.0 !");
+}
+if(marks <= 60 && marks >= 57){
+    document.write("Your Grade is C- ");
+    document.write("Your GPA IS 1.8 !");
+}
+if(marks <= 56 && marks >= 53){
+    document.write("Your Grade is D+ ");
+    document.write("Your GPA IS 1.4 !");
+}
+if(marks <= 50 && marks >= 52){
+    document.write("Your Grade is D ");
+    document.write("Your GPA IS 1.0 !");
+}
+if(marks <= 49){
+    document.write("You are Fail ");
+    document.write("Your GPA IS 0.0 !");
+}
